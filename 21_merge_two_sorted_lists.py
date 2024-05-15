0736b063a4c029b71b088186bddced4037f1c669
Herl, Makenna <mherl@hrl.com>
2024-05-15 16:11:59 -0700

    recursive nodes

--- 21_merge_two_sorted_lists.py
+++ 21_merge_two_sorted_lists.py
@@ -3,11 +3,9 @@ def main():
     list2 = [1, 3, 4]
     node_list_1 = list_to_list_of_list_nodes(list1)
     node_list_2 = list_to_list_of_list_nodes(list2)
-    print(mergeTwoLists(node_list_1, node_list_2))
 
 
 def list_to_list_of_list_nodes(list_of_nums: list):
-    list_of_list_nodes = []
     for index, num in enumerate(list_of_nums):
         try:
             node = ListNode()
@@ -19,7 +17,7 @@ def list_to_list_of_list_nodes(list_of_nums: list):
             node.val = num
             node.next = None
             return node
-    return list_of_list_nodes
+    return node
 
 
 class ListNode:
