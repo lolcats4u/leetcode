db3bc8e4439a48268b2485297d94ff0b63de399c
Herl, Makenna <mherl@hrl.com>
2024-05-15 16:22:11 -0700

    Woops

--- 21_merge_two_sorted_lists.py
+++ 21_merge_two_sorted_lists.py
@@ -1,11 +1,11 @@
 def main():
     list1 = [1, 2, 4]
     list2 = [1, 3, 4]
-    node_list_1 = list_to_list_of_list_nodes(list1)
-    node_list_2 = list_to_list_of_list_nodes(list2)
+    node_list_1 = list_to_node(list1)
+    node_list_2 = list_to_node(list2)
 
 
-def list_to_list_of_list_nodes(list_of_nums: list):
+def list_to_node(list_of_nums: list):
     if len(list_of_nums) == 1:
         node = ListNode()
         node.val = list_of_nums[0]
@@ -13,7 +13,7 @@ def list_to_list_of_list_nodes(list_of_nums: list):
     else:
         node = ListNode()
         node.val = list_of_nums[0]
-        node.next = list_of_nums[1:]
+        node.next = list_to_node(list_of_nums[1:])
     return node
 
 
