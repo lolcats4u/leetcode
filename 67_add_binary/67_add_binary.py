184b262be474cd3e7d6d0bc4c74a850796eda8cd
Herl, Makenna <mherl@hrl.com>
2024-08-09 14:12:00 -0700

    Wow

--- 67_add_binary/67_add_binary.py
+++ 67_add_binary/67_add_binary.py
@@ -1,7 +1,8 @@
 def main():
     test1 = ["11", "1"]
     test2 = ["1010", "1011"]
-    tests = [test1, test2]
+    test3 = ["10101011", "1010"]
+    tests = [test1, test2, test3]
     for test in tests:
         Solution().addBinary(test[0], test[1])
 
@@ -20,7 +21,11 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         biggest_str, smallest_str = self.determine_biggest(a, b)
         self.find_bin_sum(biggest_str, smallest_str)
-        return self.sum
+
+        if self.sum[0] == "0":
+            return self.sum[1:]
+        else:
+            return self.sum
 
     def find_bin_sum(self, biggest_str: str, smallest_str: str):
         ##### opportunities for early exit
@@ -47,14 +52,14 @@ class Solution:
             # diff should be positive
             if self.diff != 0:
                 start = -1
-                stop = -(self.smaller_len)
+                stop = -(self.smaller_len + 1)
 
             else:
                 start = -1
-                stop = -(self.bigger_len)
+                stop = -(self.bigger_len + 1)
 
             # iterate from the back until you've gone over the whole length of the smallest str
-            for i in range(start, stop):
+            for i in range(start, stop, -1):
                 self.cast_bin_int_bin(int(biggest_str[i]), int(smallest_str[i]))
 
             # handle last carry digit
@@ -65,7 +70,7 @@ class Solution:
             # append remaining digits
             else:
                 self.cast_bin_int_bin(0, int(biggest_str[-(self.smaller_len + 1)]))
-                self.sum = biggest_str[0 : self.diff] + self.sum
+                self.sum = biggest_str[0 : self.diff - 1] + self.sum
                 return
 
     def cast_bin_int_bin(self, num1: int, num2: int):
