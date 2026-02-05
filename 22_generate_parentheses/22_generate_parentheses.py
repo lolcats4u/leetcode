528f34da5222b742ea61ae1fe3c9aab98e988056
Makenna Herl <hire.makenna.herl@gmail.com>
2026-02-05 11:19:54 -0800

    Ugg.

---
+++ 22_generate_parentheses/22_generate_parentheses.py
@@ -0,0 +1,47 @@
+from typing import List
+from copy import copy
+
+def main(test):
+    solution = Solution().generateParenthesis(test)
+    print(solution)
+class Solution:
+    def __init__(self):
+        self.parentheses_options = []
+        self.count = None
+    def generateParenthesis(self, n: int) -> List[str]:
+        if self.count !=0:
+            if not self.count:
+                self.count = n
+        count_case_1 = copy(n)
+        count_case_2 = copy(n)
+        def case_1(n,paren_option, options):
+                if n != 0:
+                    n -=1
+                    paren_option_1 += f"({self.generateParenthesis(n)})"
+                    paren_option_2 += case_2(n, paren_option_1, options)
+
+                else: 
+                    options.append(paren_option_1, paren_option_2)
+        def case_2(n, paren_option, options):
+            if n != 0:
+                n -= 1
+                paren_option_1 += f"(){self.generateParenthesis(n)}"
+                paren_option_2 += case_1(n, paren_option_1, options)
+        
+        while self.count != 0:
+            case_1 = case_1(count_case_1, "", self.parentheses_options)
+            case_2 = case_2(count_case_2, "", self.parentheses_options)
+            if count_case_1 == 0 and count_case_2 == 0:
+                self.count = 0
+
+        self.parentheses_options = [case_1, case_2]
+
+def tests():
+    test_1 = 3
+    test_2 = 1
+    return locals().values()
+
+if __name__ == "__main__":
+    tests = tests()
+    for test in tests:
+        main(test)
