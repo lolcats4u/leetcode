46dbb26f5099a1a941c822390dacf865e14208b5
Makenna Herl <hire.makenna.herl@gmail.com>
2026-02-05 12:31:19 -0800

    Added lower level count check.

--- 22_generate_parentheses/22_generate_parentheses.py
+++ 22_generate_parentheses/22_generate_parentheses.py
@@ -8,17 +8,18 @@ class Solution:
     def __init__(self):
         self.parentheses_options = []
     def generateParenthesis(self, n: int) -> List[str]:
-        def case_1(parenthesis_string:str) -> str:
-            return (parenthesis_string + f"({case_1(parenthesis_string, n)})")
+        def case_1(parenthesis_string:str, count:int) -> str:
+            if len(parenthesis_string) != (2*count):
+                return (parenthesis_string + f"({case_1(parenthesis_string, n)})")
 
-        def case_2(parenthesis_string) -> str:
-            return (parenthesis_string + f"(){case_2(parenthesis_string, n)}")
+        def case_2(parenthesis_string, count:int) -> str:
+            if len(parenthesis_string) != (2*count): 
+                return (parenthesis_string + f"(){case_2(parenthesis_string, n)}")
         
         def branch_case(parenthesis_string:str, count, parenthesis_options:list, *cases):
             for case in cases:
-                while len(parenthesis_string) != (count *2):
-                    parenthesis_string += branch_case(case(parenthesis_string), count, parenthesis_options, cases)
-                parenthesis_options.append(parenthesis_string)
+                parenthesis_string += branch_case(case(parenthesis_string), count, parenthesis_options, cases)
+            parenthesis_options.append(parenthesis_string)
         
         branch_case("", n, self.parentheses_options, case_1, case_2)
 
