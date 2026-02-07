019cf79d4214bf28aef70004e2fbf58cc5868346
Makenna Herl <hire.makenna.herl@gmail.com>
2026-02-07 11:37:48 -0800

    ugg

--- 22_generate_parentheses/22_generate_parentheses.py
+++ 22_generate_parentheses/22_generate_parentheses.py
@@ -1,31 +1,18 @@
 from typing import List
-from copy import copy
 
 def main(test):
     solution = Solution().generateParenthesis(test)
     print(solution)
 class Solution:
     def __init__(self):
-        self.parentheses_options = []
+        self.parentheses_options = [""]
     def generateParenthesis(self, n: int) -> List[str]:
-        def case_1(parenthesis_string:str, count:int) -> str:
-            if len(parenthesis_string) != (2*count):
-                return (parenthesis_string + f"({case_1(parenthesis_string, n)})")
-            else:
-                return parenthesis_string
+        recursion_depth = 0
+        while recursion_depth != n:
+            pass
 
-        def case_2(parenthesis_string, count:int) -> str:
-            if len(parenthesis_string) != (2*count): 
-                return (parenthesis_string + f"(){case_2(parenthesis_string, n)}")
-            else:
-                return parenthesis_string
-        
-        def branch_case(parenthesis_string:str, count, parenthesis_options:list, *cases):
-            for case in cases:
-                branch_case(case(parenthesis_string), count, parenthesis_options, cases)
-            parenthesis_options.append(parenthesis_string)
-        
-        branch_case("", n, self.parentheses_options, case_1, case_2)
+
+                        
 
 def tests():
     test_1 = 3
