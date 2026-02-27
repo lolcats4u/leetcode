b41ae074b78d42652c1c2c7c0e36222df09c8cbd
Makenna Herl <hire.makenna.herl@gmail.com>
2026-02-27 12:30:16 -0800

    Implement a stack.

---
+++ 155_min_stack/155_min_stack.py
@@ -0,0 +1,58 @@
+
+def main(test):
+    for test_operation in test:
+        if test_operation[0] == "MinStack":
+            solution = MinStack()
+            continue
+        if test_operation[0] == "push":
+            solution.push(test_operation[1])
+            continue
+        if test_operation[0] == "pop":
+            solution.pop()
+            continue
+        if test_operation[0] == "top":
+            top = solution.top()
+            print(top)
+            continue
+        if test_operation[0] == "getMin":
+            min = solution.getMin()
+            print(min)
+            continue
+
+    print(solution.stack)
+    return solution.stack
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack = [val] + self.stack
+
+    def pop(self) -> None:
+        self.stack = self.stack[1:]
+
+
+    def top(self) -> int:
+        return self.stack[0]
+        
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+
+def tests():
+    test_0 = zip(["MinStack","push","push","push","getMin","pop","top","getMin"], [[],[-2],[0],[-3],[],[],[],[]])
+    return locals().values()
+
+if __name__ == "__main__":
+    tests = tests()
+    for test in tests:
+        main(test)
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
